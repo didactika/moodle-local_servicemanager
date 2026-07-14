d1349fbc82a1aaa466d9ba83a57c6f2aa633ffdf
Eduardo Estrada <me@e2rd0.com>
2026-07-14 09:33:26 +0200

    fix: add moodle boilerplate header comments on amd source files

--- amd/src/health_log_filter.js
+++ amd/src/health_log_filter.js
@@ -1,12 +1,27 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * Health logs filter for Service Schema view page.
  *
- * @module     local_servicemanager/health_log_filter
+ * @author     Eduardo Estrada <me@e2rd0.com>
+ * @author     Hector Arrechea
  * @copyright  2026 Didactika.org
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @module     local_servicemanager/health_log_filter
  */
-
-import $ from 'jquery';
 
 /**
  * Initialize the filter.
