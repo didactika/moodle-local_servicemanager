5654a766a3ca4a887bd0969b0ed9a6be536c370c
Eduardo Estrada <me@e2rd0.com>
2026-07-20 12:41:57 +0200

    fix: set supported Moodle range to 4.5–5.2 (5.3 not released yet)

--- version.php
+++ version.php
@@ -31,4 +31,4 @@ $plugin->version = 2026071400;
 $plugin->requires = 2024100700; // Moodle 4.5+.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.1';
-$plugin->supported = [405, 503]; // Moodle 4.5 to 5.3.
+$plugin->supported = [405, 502]; // Moodle 4.5 to 5.2.
